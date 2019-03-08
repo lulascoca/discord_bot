f35d12549f25089010dcfc8aad2fb5dea2b3413c
root <lulascocainfinity@gmail.com>
2019-03-08 20:27:17 +0000

    added weather functionality

--- bot.py
+++ bot.py
@@ -9,22 +9,29 @@ import random
 import requests
 import json
 import io
+import modules.req as r
 
+# prints the discord version
 print("Discord version: %s" % discord.__version__)
 
+# this function takes a file named token.txt and reads the contents then passing it to the bot when called (returning)
 def read_token():
 	with open("files/token.txt", "r") as f:
 		lines = f.readlines()
 		return lines[0].strip()
 
+# using the above function to pass the token into the variable TOKEN
 TOKEN = read_token()
 
+# create bot/client "object" with command prefix !
 bot = commands.Bot(command_prefix = "!")
 
+# dunno what this is I was gonna do smthg but forgot what XD
 class Main_commands():
 	def __init__(self, bot):
 		self.bot = bot
 
+# just in case I need to get a channel :)
 def get_channel(channels, channel_name):
     for channel in bot.get_all_channels():
         print(channel)
@@ -32,10 +39,27 @@ def get_channel(channels, channel_name):
             return channel
     return None
 
+# print when the bot is ready
 @bot.event
 async def on_ready():
 	print("ready\n")
 
+@bot.command(pass_context = True)
+async def weather(ctx, name="", id=0):
+	a = r.search_city(name, id)
+	print(str(a))
+	if a == None:
+		await bot.say("Couldn't find that place sry m8 :heart:")
+	else:
+		weath = r.get_weather(a)
+		print(weath)
+		ls = weath["list"]
+		m = ls[0]
+		main = m["main"]
+		temp = main["temp"]
+		await bot.say("The currnet temperature in " + name + " is currently " + str(temp))
+
+# basic ping command
 @bot.command()
 async def ping():
 	""" Pong! """
@@ -44,14 +68,17 @@ async def ping():
 	ping = (time.monotonic() - before) * 1000
 	await bot.say("This took " + str(ping) + " ms")
 
+# basic image test command might use this as a base to future better commands
 @bot.command(pass_context = True)
 async def pepe(ctx):
 	await bot.send_file(ctx.message.channel, open("images/dank_pepe_or_nah_by_sheepjesus-d9khyt8.png", "rb"))
 
+# basic gif sending command
 @bot.command(pass_context = True)
 async def gif(ctx):
         await bot.send_file(ctx.message.channel, open("images/a.gif", "rb"))
 
+# basic echo command that repeats user input
 @bot.command()
 async def echo(*message):
 	output = ""
@@ -68,5 +95,6 @@ async def porn():
 		await bot.send_file(get_channel(bot.get_all_channels(),channel_name="general", pic)
 """
 
+# actual bot running :)
 bot.run(TOKEN)
 
--- modules/req.py
+++ modules/req.py
@@ -4,37 +4,44 @@ import requests
 import sys
 import configparser
 import json
+import urllib.request
 
 #cofnig = configparser.ConfigParser()
 
-r = requests.get("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=0c0f9be83b03974dc0e892436eaa9a13")
-
-with open("city.list.json", "r") as file:
-	data = json.load(file)
-
-for f in data:
-     a = f["name"]
-     if a == "Viseu":
-             print(a)
+#r = requests.get("http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=0c0f9be83b03974dc0e892436eaa9a13")
 
 
+def search_city(name="", id=0):
+	with open("files/city.list.json", "r") as file:
+		data = json.load(file)
+	try:
+		for f in data:
+			cit_name = f["name"]
+			cit_id = f["id"]
+			if cit_name.lower() == name.lower() and not cit_name == "":
+				for key in f:
+					print(key + " - " + str(f[key]))
+				print("\n")
+				return cit_id
+			elif cit_id == id:
+				for key in f:
+					print(key + " - " + str(f[key]))
+				print("\n")
+				return cit_id
+	except Exception as e:
+		print("unable to locate city/place")
 
-
+def get_weather(a):
+	url = ("http://api.openweathermap.org/data/2.5/forecast?id=" + str(a) + "&APPID=0c0f9be83b03974dc0e892436eaa9a13" + "&units=metric")
+	print(url)
+	with urllib.request.urlopen(url) as url_:
+		data = json.loads(url_.read().decode())
+		return data
 
 """
-with open("city.list.json") as json_file:
-    #text = json_file.read()
-    json_data = json.loads(json_file)
-    print(json_data)
-"""
-
-#json_data = json.loads("city.list.json")
-
-#sample
-#r = requests.get("https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1")
-
 print(r.status_code)
 
 for key in r.headers:
 	print(key + ": ")
 	print(r.headers[key] + "\n")
+"""
