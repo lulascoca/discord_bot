e78498ab6386481fea8579ce6835986283555f67
lulaz <lulascocainfinity@gmail.com>
2019-03-12 21:56:59 +0000

    fixed small error with emojis where there was no match for night conditions

--- modules/json__dump_dict.py
+++ modules/json__dump_dict.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import json
 
 icons = {
@@ -9,7 +11,16 @@ icons = {
 		"10d": ":white_sun_rain_cloud:️",
 		"11d": ":cloud_lightning:️",
 		"13d": ":cloud_snow:️",
-		"50d": ":fog:️"
+		"50d": ":fog:️",
+		"01n": ":sunny:️", 
+		"02n": ":partly_sunny:", 
+		"03n": ":cloud:️",
+		"04n": ":cloud::cloud:",
+		"09n": ":cloud_rain:️",
+		"10n": ":white_sun_rain_cloud:️",
+		"11n": ":cloud_lightning:️",
+		"13n": ":cloud_snow:️",
+		"50n": ":fog:️"
 		}
 
 with open("../files/icons.json", "w") as file:
